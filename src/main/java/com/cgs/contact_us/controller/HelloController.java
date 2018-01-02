3df2862267f3e96f769ab2299a18b1b4d276b478
hengsheng <1055038041@qq.com>
2018-01-02 17:45:30 +0800

    实现打开页面

--- src/main/java/com/cgs/contact_us/controller/HelloController.java
+++ src/main/java/com/cgs/contact_us/controller/HelloController.java
@@ -1,14 +1,19 @@
 package com.cgs.contact_us.controller;
 
+import com.cgs.contact_us.bean.Contact;
 import com.cgs.contact_us.service.impl.ContactServiesImpl;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController
+import java.util.List;
+
+@Controller
 @EnableAutoConfiguration
 @RequestMapping("/contact")
 public class HelloController {
@@ -16,14 +21,17 @@ public class HelloController {
     @Autowired
     private ContactServiesImpl contactServies;
 
+    @ResponseBody
     @RequestMapping("/hello")
     public String helloworke(){
         return "hello";
     }
 
-    @RequestMapping(method = RequestMethod.GET)
-    public String getAll(ModelMap modelmap){
-        modelmap.addAttribute("data",contactServies.getAll());
-        return "list";
+    @RequestMapping("/list")
+    public String getAll(ModelMap model){
+        List<Contact> contactList = contactServies.getAll();
+        model.addAttribute("data",contactList);
+        model.addAttribute("id", contactList.get(0).getId());
+        return "contact/list";
     }
 }
